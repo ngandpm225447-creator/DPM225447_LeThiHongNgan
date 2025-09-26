1a64795bb5d9991165980a8951cd9615f86f49c7
ngandpm225447-creator <ngan_dpm225447@student.agu.edu.vn>
2025-09-26 08:28:01 +0700

    Add files via upload

---
+++ DPM225447_LeThiHongNgan_pattern02_Builder/Director.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Director
+{
+    // Builder uses a complex series of steps
+
+    public void Construct(Builder builder)
+    {
+        builder.BuildPartA();
+        builder.BuildPartB();
+    }
+}
---
+++ DPM225447_LeThiHongNgan_pattern02_Builder/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class MainApp
+{
+
+    public static void Main()
+    {
+        // Create director and builders
+
+        Director director = new Director();
+
+        Builder b1 = new ConcreteBuilder1();
+        Builder b2 = new ConcreteBuilder2();
+
+        // Construct two products
+
+        director.Construct(b1);
+        Product p1 = b1.GetResult();
+        p1.Show();
+
+        director.Construct(b2);
+        Product p2 = b2.GetResult();
+        p2.Show();
+
+        // Chờ người dùng
+
+        Console.ReadKey();
+    }
+}
