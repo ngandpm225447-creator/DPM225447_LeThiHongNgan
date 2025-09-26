ffd09012d0e03b48ddb5caeb891aad05da7ac979
ngandpm225447-creator <ngan_dpm225447@student.agu.edu.vn>
2025-09-26 08:22:06 +0700

    Add files via upload

---
+++ Buoi1/AbstractFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+abstract class AbstractFactory
+{
+    public abstract AbstractProductA CreateProductA();
+    public abstract AbstractProductB CreateProductB();
+}
---
+++ Buoi1/ProductB2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class ProductB2 : AbstractProductB
+{
+    public override void Interact(AbstractProductA a)
+    {
+        Console.WriteLine(this.GetType().Name +
+          " interacts with " + a.GetType().Name);
+    }
+}
+
